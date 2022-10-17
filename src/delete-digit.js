0a32e10d50c0ce6dd6f4908ce280fcc5c7fd0fd2
vKuptsova <kuptsowa.vika@gmail.com>
2022-10-17 21:29:31 +0300

    feat: add code in delete-digit.js

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function deleteDigit(n) {
+  let arr = String(n).split("");
+  let length = arr.length;
+  let middle = Math.ceil(length/2);
+  let arr1 = [];
+  let arr2 = [];
+  if (length % 2 !== 0){
+      for (let i = 0; i<middle; i++){
+      arr1[i] = arr[i]; 
+  }
+      for (let i = 0; i<middle; i++){
+      arr2[i] = arr[i+middle-1];
+    }
+  } else {
+      for (let i = 0; i<middle; i++){
+      arr1[i] = arr[i]; 
+  }
+      for (let i = 0; i<middle; i++){
+      arr2[i] = arr[i+middle];
+    }
+  }
+  let minNum1 = Math.min.apply(null, arr1);
+  let minNum2 = Math.min.apply(null, arr2);
+  for (let i = 0; i<arr.length; i++){
+    if ((minNum1 === minNum2 || minNum1 < minNum2) || (arr[middle-1] > arr[0])) {
+        if (Number(arr[i]) === minNum1){
+        arr.splice(i, 1);
+        break;
+        }
+    } else {
+        if (Number(arr[i]) === minNum2){
+        arr.splice(i, 1);
+        break;
+        }
+    }
+  }
+  
+  return Number(arr.join(""));
 }
 
 module.exports = {
